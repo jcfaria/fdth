43cfa2377291edae8608d4227d01002a0da2977a
jcfaria <joseclaudio.faria@gmail.com>
2013-07-18 11:20:55 -0300

    plot.fdt.multiple: x11() was replaced by dev.new()

--- R/fdt.matrix.R
+++ R/fdt.matrix.R
@@ -1,32 +1,33 @@
-fdt.matrix <-
-  function (x,
-            k,
-            breaks=c('Sturges', 'Scott', 'FD'),
-            right=FALSE, ...)
-  {
-    stopifnot(is.matrix(x))
-    res <- list()
+fdt.matrix <- function (x,
+                        k,
+                        breaks=c('Sturges', 'Scott', 'FD'),
+                        right=FALSE, ...)
+{
+  stopifnot(is.matrix(x))
+  res <- list()
 
-    for (i in 1:ncol(x)) {
-      m      <- as.matrix(x[ ,i])
-      fdt    <- make.fdt.multiple(m,
-                                  k,
-                                  breaks,
-                                  right)
-      tmpres <- list(table=fdt[[1]],
-                     breaks=fdt[[2]])
-      res    <- c(res,
-                  list(tmpres))
-    }
+  for (i in 1:ncol(x)) {
+    m <- as.matrix(x[ ,i])
+    fdt <- make.fdt.multiple(m,
+                             k,
+                             breaks,
+                             right)
+    tmpres <- list(table=fdt[[1]],
+                   breaks=fdt[[2]])
+    res <- c(res,
+             list(tmpres))
+  }
 
-    nms <- if (is.null(colnames(x))) paste('Column',
-                                           1:ncol(x),
-                                           sep=':')
-  else colnames(x)
+  if (is.null(colnames(x)))
+    nms <- paste('Column',
+                 1:ncol(x),
+                 sep=':')
+  else
+    nms <- colnames(x)
 
   names(res) <- nms
   class(res) <- c('fdt.multiple',
                   'list')
   invisible(res)
-  }
+}
 
